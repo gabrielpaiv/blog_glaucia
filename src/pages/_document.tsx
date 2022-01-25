5704d163afedd8749f7d23301bf29e6249a1fe20
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2022-01-25 08:40:49 -0300

    Created Next App

---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+          <script
+            async
+            defer
+            src="https://static.cdn.prismic.io/prismic.js?new=true&repo=site-glaucia"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
