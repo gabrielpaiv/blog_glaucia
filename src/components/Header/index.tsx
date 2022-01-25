4ac9df94db177820a817a0cc3fd64aadca8fade9
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2022-01-25 09:34:04 -0300

    Added Images and Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+import styles from './header.module.scss'
+
+export default function Header() {
+  return (
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <Link href="/">
+          <a>
+            <img src="/images/logo.png" alt="logo" />
+          </a>
+        </Link>
+        <h1>Glaucia Cavalcanti</h1>
+      </div>
+    </header>
+  )
+}
